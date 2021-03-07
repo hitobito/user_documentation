956196689fe3f37727fd70e9e6e7022e88f3fe68
Urs Roesch <github@bun.ch>
2021-03-07 13:07:33 +0100

    Add transifex configurations and po files
    Summary:
      * Configure transifex for the .rst files
      * Modify conf.py to define the translations directory

--- conf.py
+++ conf.py
@@ -61,6 +61,8 @@ master_doc = 'index'
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
 language = u'de'
+locale_dir = ["locales/"]
+gettext_compact = False
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
